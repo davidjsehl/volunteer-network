4c0b72f0d316c1c27407f6b8701fd2a126b8cf47
David Sehl <davidjsehl@gmail.com>
2018-09-04 14:07:57 -0400

    chore: add logo to navbar

--- client/components/main.js
+++ client/components/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
     Collapse,
+    Media,
     Navbar,
     NavbarToggler,
     NavbarBrand,
@@ -34,41 +35,37 @@ class Main extends Component {
     }
 
     render () {
-        return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/events/">Events</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="/organizations/">Organizations</NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Sign Up/Log In
-                                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                        Individual
-                                    </DropdownItem>
-                                    <DropdownItem>
-                                        Organization
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Log In
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        )
+        return <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">
+                <Media style={{ maxWidth: '180px' }} object src="VNLogo.png" />
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/events/">Events</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/organizations/">
+                      Organizations
+                    </NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Sign Up/Log In
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Individual</DropdownItem>
+                      <DropdownItem>Organization</DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>Log In</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>;
     }
 }
 
