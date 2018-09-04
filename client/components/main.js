fe573a4ac7e810c3a6d129c01d6892cde3e08ee9
David Sehl <davidjsehl@gmail.com>
2018-09-04 13:06:09 -0400

    feat: add navbar

--- client/components/main.js
+++ client/components/main.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
 
 class Main extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
     render () {
         return (
             <div>
-                Hello this is my main component
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/events/">Events</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/organizations/">Organizations</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Sign Up/Log In
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Individual
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Organization
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Log In
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
             </div>
         )
     }
